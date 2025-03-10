661d0f6c641599c6f614d3a32fd07662d90da2ff
abhijeet <abhijeet.jena64@gmail.com>
2025-03-10 22:34:23 +0530

    09 MarchI am pushing the code

---
+++ src/ex_01_javabasics/Lab03_multiplecomments.java
@@ -0,0 +1,9 @@
+package ex_01_javabasics;
+
+public class Lab03_multiplecomments {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Abhijeet");
+        System.out.println("Sabita");
+    }
+}
