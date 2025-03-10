7a3a6b20ab449f02149b62b990a66c4bfc4522d4
abhijeet <abhijeet.jena64@gmail.com>
2025-03-10 22:36:00 +0530

    09 MarchI am pushing the code

--- src/ex_01_javabasics/Lab002_helloworldprogram2.java
+++ src/ex_01_javabasics/Lab002_helloworldprogram2.java
@@ -3,5 +3,6 @@ package ex_01_javabasics;
 public class Lab002_helloworldprogram2 {
     public static void main(String[] args) {
         System.out.println("Hello world program atb");
+        System.out.println();
     }
 }
