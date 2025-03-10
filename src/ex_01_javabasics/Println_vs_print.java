a55730cd5044d8296b0da2db74068f3006e2bc39
abhijeet <abhijeet.jena64@gmail.com>
2025-03-10 22:43:12 +0530

    10 MarchI am pushing the code

---
+++ src/ex_01_javabasics/Println_vs_print.java
@@ -0,0 +1,10 @@
+package ex_01_javabasics;
+
+public class Println_vs_print {
+    public static void main(String[] args) {
+       // System.out.println("Hello world abhijeet CTS");
+      //  System.out.println("Real world");
+        System.out.print("Abhijeet ACCEN");
+        System.out.print("KP Jena");
+    }
+}
